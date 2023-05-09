c9cbe0885f33ccb34ac64ae31b7eae639cd83cea
Vivinat <venat2044@gmail.com>
2023-05-09 08:17:25 -0300

    LOGIN
    Ao entrar na raiz RAIZ: http://localhost:3000/ é feita uma verificação com o servidor, usando o cookie armazenado.
    SEM BISCOITO OU BISCOITO NÃO CONFERE: VÁ PARA SANDBOX.HTML
    COM BISCOITO: NADA ACONTECE (Por enquanto)

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
-const {MongoClient, ObjectId} = require('mongodb');
+const {MongoClient} = require('mongodb');
 const express = require('express');
+const {ObjectId} = require('mongodb');
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -11,6 +12,7 @@ async function main(){
     /**
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * http://localhost:3000/public/sandbox.html
+     * RAIZ: http://localhost:3000/
      */
     const uri = "mongodb+srv://venat:bAsEwW6b3gDP86j8@sorestonoiscluster.urpzo2g.mongodb.net/?retryWrites=true&w=majority";
 
@@ -21,7 +23,6 @@ async function main(){
         await client.connect();
         console.log("Connected to MongoDB Atlas");
 
-
         app.post('/register', async (req, res) => {
             const name = req.body.name;
             const bullets = 0;
@@ -30,13 +31,49 @@ async function main(){
             try {
                 const result = await registerUser(client, name, bullets, progression); 
                 console.log(`User registered successfully with ID ${result}.`);
-                res.cookie('userId', result.toString());
+                res.cookie('userId', result.toString(), { expires: new Date('2023-12-31T23:59:59Z') });
                 res.status(302).setHeader('Location', 'http://localhost:3000/public/choices.html').end();
             } catch (error) {
                 console.error(error);
                 res.sendStatus(500);
             }
         });
+
+        app.get('/', async (req, res) => {
+            const userId = req.cookies.userId;
+            if (!userId) {
+                res.sendFile(__dirname + '/public/sandbox.html');
+                return;
+            }
+
+            try {
+                const user = await findUser(client, userId);
+                if (user) {
+                    res.send(`
+                    <html>
+                    <head>
+                      <title>Bem-vindo, ${user.newName}!</title>
+                      <link rel="stylesheet" href="/public/style.css">
+                    </head>
+                    <body>
+                      <h1>Bem-vindo, ${user.newName}!</h1>
+                      <p>Progresso: ${user.starterProgression}</p>
+                      <p>Balas: ${user.starterBullets}</p>
+                      <button id="LogButton">Continuar</button>
+                      <button id="NewGameButton"><a href="/public/sandbox.html">Novo Jogo</a></button>
+                    </body>
+                    </html>
+                    `);
+                } else {
+                    res.clearCookie('userId');
+                    res.sendFile(__dirname + '/public/sandbox.html');
+                }
+            } catch (error) {
+                console.error(error);
+                res.sendStatus(500);
+            }
+        });
+
 
         app.listen(3000, () => {
             console.log('Server started at port 3000');
@@ -59,6 +96,11 @@ async function registerUser(client, newName, starterBullets, starterProgression)
     console.log(`User ${newName} registered successfully.`);
     return result.insertedId;
 }
+
+async function findUser(client, userId) {
+    return client.db("PlayerStats").collection("_stats").findOne({ _id: new ObjectId(userId) });
+}
+
 
 
 async function listDatabases(client){
