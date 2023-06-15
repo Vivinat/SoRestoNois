67f8905457b35565c6fa4cf47efb125fd59a8bba
Matheus Rodrigues Pronunciate <pronun2m@hotmail.com>
2023-06-15 00:12:19 -0300

    Janelas para o registro & um pouco de css

--- so-resto-nois-front/src/Register.jsx
+++ so-resto-nois-front/src/Register.jsx
@@ -1,4 +1,29 @@
 export default function Register(){
+    
+    const form = document.querySelector('#register-form');
+    const nameInput = document.querySelector('#name-input');
+    const logButton = document.getElementById('LogButton');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+    //Aqui verifica o forms de registro.
+    const name = nameInput.value;
+    const bullets = 0;
+    const progression = 'N1A';
+
+    //Usuario se registrou. Mande para server.js fazer a conexão
+    const response = await fetch('/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, bullets, progression })
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+
+});
+    
     return (
         <>
         <h1>Cadastro de usuário</h1>
