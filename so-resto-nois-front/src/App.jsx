437bf230913f07765c6e3136e404127fbe7d2a61
Matheus Rodrigues Pronunciate <pronun2m@hotmail.com>
2023-06-26 23:21:41 -0300

    modificações para aplicar router

--- so-resto-nois-front/src/App.jsx
+++ so-resto-nois-front/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Game from './Game'
-import Register from './register'
+import Game from '../components/Game'
+import Register from '../components/Register'
 
 function App() {
   const [count, setCount] = useState(0)
--- so-resto-nois-front/src/main.jsx
+++ so-resto-nois-front/src/main.jsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+//importando rotas
+import Register from '../components/Register.jsx';
+import Game from '../components/Game.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/', //pagina home para registrar usuario
+    element: <Register />
+  },
+  {
+    path: '/game',
+    element: <Game />
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
