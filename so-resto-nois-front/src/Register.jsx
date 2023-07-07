0a53d4e0c11e59f492f04ee115512463bee4193b
MatheusPronunciate <pronun2m@hotmail.com>
2023-07-07 17:04:56 -0300

    Adição do hookForm e alguns testes

--- so-resto-nois-front/src/Register.jsx
+++ so-resto-nois-front/src/Register.jsx
@@ -1,14 +1,48 @@
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+
 export default function Register(){
+
+    const form = useForm({
+        resolver: yupResolver(schema)
+    });
+
+    const { register, handleSubmit, formState } = form;
+
+    const {errors} = formState;
+
+    const submit = async (data) => {
+        console.log('dados enviados', data);
+        console.log('erro', errors);
+        axios.post('http://localhost:3000/register', data)
+            .then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
     return (
         <>
         <h1>Cadastro de usuário</h1>
 
-        <form action="/register" method="POST">
+        <form onSubmit={handleSubmit(submit)} noValidate>
+            <label htmlFor="name-input">Nome:</label>
+            <input type="text" name="name" id="name-input" {...register('name-input')} />
+            <p className='erro'>{errors.name?.message}</p>
+            
+            <button type="submit">Enviar</button>
+        </form>
+
+        {/*<form action="/register" method="POST">
         <label> Nome:
             <input type="text" name="name" id="name-input"/>
         </label>
         <button type="submit">Enviar</button>
-        </form>
+        </form>*/}
 
 
         <footer><p>Utilizamos cookies para facilitar o armazenamento!</p></footer>
