2f2af36e53c03ebdaa749430ffe0088c5ab05033
Matheus Rodrigues Pronunciate <pronun2m@hotmail.com>
2023-07-10 00:33:40 -0300

    let current heath

--- so-resto-nois-front/src/GameResources/PlayerHealth.jsx
+++ so-resto-nois-front/src/GameResources/PlayerHealth.jsx
@@ -3,22 +3,25 @@ import axios, { AxiosHeaders } from "axios";
 
 export default function PlayerHealth() {
     
-    const [currentHealth, setCurrentHealth] = useState([]);
-    const [totalHealth, setTotalHealth] = useState([]);
+    //const [currentHealth, setCurrentHealth] = useState([]);
+    //const [totalHealth, setTotalHealth] = useState([]);
 
+
+    
 
     useEffect(() => {
         axios.get('/api/updateHealth')
         .then((response) => {
             setCurrentHealth(response.data.currentHealth);
-            console.log(response.data);
+            console.log(response.data.currentHealth);
         })
         .catch((error) => {
             console.log(error);
         });
     }, []);
 
-   
+   let currentHealth = 3;
+   let totalHealth = 5;
 
     return(
         <>
