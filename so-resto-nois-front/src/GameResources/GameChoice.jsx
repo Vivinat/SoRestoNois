1347a13e617c61c0f21e7c1d95bbbb12f107b2dd
Vivinat <venat2044@gmail.com>
2023-07-14 14:17:32 -0300

    Apagando Front-End
    Me perdoe Matheus

--- so-resto-nois-front/src/GameResources/GameChoice.jsx
+++
@@ -1,6 +0,0 @@
-export default function GameChoice() {
-    let ChoiceText = 'o Mofo...'
-    return(
-        <button>{ChoiceText}</button>
-    )
-}
--- so-resto-nois-front/src/GameResources/PlayerBullets.jsx
+++
@@ -1,9 +0,0 @@
-export default function PlayerBullets() {
-    let balas = 0;
-    
-    return(
-        <p>
-            numero de balas: {balas}
-        </p>
-    )
-}
